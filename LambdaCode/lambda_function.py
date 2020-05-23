bbff6489ea76e800cc54271b50f36f13bdc7ce99
Tapsmoth <65776177+Tapsmoth@users.noreply.github.com>
2020-05-23 18:12:14 +0530

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -5,5 +5,5 @@ ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     response = ec2.describe_availability_zones()
     ack = "The Deployment is Sucessful xD"
-    json.dumps(ack)
-    return {"statusCode": 200, "body": json.dumps(response)}
+    print(ack)
+    return {"statusCode": 200, "body": json.dumps(response),"message":ack}
